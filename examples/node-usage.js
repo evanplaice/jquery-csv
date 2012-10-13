a48f147b6157c88d02c14c59a50663bc99f80a73
evanplaice <evanplaice@gmail.com>
2012-10-12 23:21:03 -0700

    Renamed node-parse.js to node-usage.js

--- examples/node-usage.js
+++ examples/node-usage.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
-var $ = jQuery = require('jquery');
+var $ = jQuery = require('jQuery');
 require('../src/jquery.csv.js');
 
-var sample = './sample.csv';
+var sample = './data/sample.csv';
 fs.readFile(sample, 'UTF-8', function(err, csv) {
   $.csv.toArrays(csv, {}, function(err, data) {
     for(var i=0, len=data.length; i<len; i++) {
