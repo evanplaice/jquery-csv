6ba67eada6faad9d13473096938cbcb5af399559
evanplaice <evanplaice@gmail.com>
2012-10-24 15:23:22 -0700

    Added the 'parsers' namespace to $.csv
    jquery.csv.js
    * created a new 'parsers' namespace under 'csv'
    * moved the splitLines method into the new namespace.
    * fixed function calls referencing splitLines

--- src/jquery.csv.js
+++ src/jquery.csv.js
@@ -74,81 +74,83 @@ RegExp.escape= function(s) {
         }
       }
     },
-    
-    splitLines: function(csv, delimiter) {
-      var state = 0;
-      var value = "";
-      var line = "";
-      var lines = [];
-      function endOfRow() {
-        lines.push(value);
-        value = "";
-        state = 0;
-      };
-      csv.replace(/(\"|,|\n|\r|[^\",\r\n]+)/gm, function (m0){
-        switch (state) {
-          // the start of a value/entry
-          case 0:
-            if (m0 === "\"") {
-              state = 1;
-            } else if (m0 === "\n") {
-              endOfRow();
-            } else if (/^\r$/.test(m0)) {
-              // carriage returns are ignored
-            } else {
-              value += m0;
-              state = 3;
-            }
-            break;
-          // delimited input  
-          case 1:
-            if (m0 === "\"") {
-              state = 2;
-            } else {
-              value += m0;
-              state = 1;
-            }
-            break;
-          // delimiter found in delimited input
-          case 2:
-            // is the delimiter escaped?
-            if (m0 === "\"" && value.substr(value.length - 1) === "\"") {
-              value += m0;
-              state = 1;
-            } else if (m0 === ",") {
-              value += m0;
-              state = 0;
-            } else if (m0 === "\n") {
-              endOfRow();
-            } else if (m0 === "\r") {
-            } else {
-              throw new Error("Illegal state");
-            }
-            break;
-          // un-delimited input
-          case 3:
-            if (m0 === ",") {
-              value += m0;
-              state = 0;
-            } else if (m0 === "\"") {
-              throw new Error("Unquoted delimiter found");
-            } else if (m0 === "\n") {
-              endOfRow();
-            } else if (m0 === "\r") {
-              // Ignore
-            } else {
-              throw new Error("Illegal data");
-            }
+
+    parsers: {
+      splitLines: function(csv, delimiter) {
+        var state = 0;
+        var value = "";
+        var line = "";
+        var lines = [];
+        function endOfRow() {
+          lines.push(value);
+          value = "";
+          state = 0;
+        };
+        csv.replace(/(\"|,|\n|\r|[^\",\r\n]+)/gm, function (m0){
+          switch (state) {
+            // the start of a value/entry
+            case 0:
+              if (m0 === "\"") {
+                state = 1;
+              } else if (m0 === "\n") {
+                endOfRow();
+              } else if (/^\r$/.test(m0)) {
+                // carriage returns are ignored
+              } else {
+                value += m0;
+                state = 3;
+              }
               break;
-          default:
-            throw new Error("Unknown state");
+            // delimited input  
+            case 1:
+              if (m0 === "\"") {
+                state = 2;
+              } else {
+                value += m0;
+                state = 1;
+              }
+              break;
+            // delimiter found in delimited input
+            case 2:
+              // is the delimiter escaped?
+              if (m0 === "\"" && value.substr(value.length - 1) === "\"") {
+                value += m0;
+                state = 1;
+              } else if (m0 === ",") {
+                value += m0;
+                state = 0;
+              } else if (m0 === "\n") {
+                endOfRow();
+              } else if (m0 === "\r") {
+              } else {
+                throw new Error("Illegal state");
+              }
+              break;
+            // un-delimited input
+            case 3:
+              if (m0 === ",") {
+                value += m0;
+                state = 0;
+              } else if (m0 === "\"") {
+                throw new Error("Unquoted delimiter found");
+              } else if (m0 === "\n") {
+                endOfRow();
+              } else if (m0 === "\r") {
+                // Ignore
+              } else {
+                throw new Error("Illegal data");
+              }
+                break;
+            default:
+              throw new Error("Unknown state");
+          }
+          return "";
+        });
+        if (state != 0) {
+          endOfRow();
         }
-        return "";
-      });
-      if (state != 0) {
-        endOfRow();
-      }
-      return lines;
+        return lines;
+      },
     },
 
     /**
@@ -289,7 +291,7 @@ RegExp.escape= function(s) {
       if(!config.experimental) {
         lines = csv.split(/\r\n|\r|\n/g);
       } else {
-        lines = $.csv.splitLines(csv, config.delimiter);
+        lines = $.csv.parsers.splitLines(csv, config.delimiter);
       }
 
       for(var i=0, len=lines.length; i<len; i++) {
@@ -349,7 +351,7 @@ RegExp.escape= function(s) {
       if(!config.experimental) {
         lines = csv.split(/\r\n|\r|\n/g);
       } else {
-        lines = this.splitLines(csv, config.delimiter);
+        lines = $.csv.parsers.splitLines(csv, config.delimiter);
       }
 
       // fetch the headers
