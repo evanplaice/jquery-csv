0ea7fe55b731023edd4f41ff501631058506c76c
evanplaice <evanplaice@gmail.com>
2012-11-04 20:44:16 -0800

    fixed castToScalar to properly return nulls instead of replacing them with 0's
    jquery.csv.js

--- src/jquery.csv.js
+++ src/jquery.csv.js
@@ -56,7 +56,7 @@ RegExp.escape= function(s) {
           } else {
             var integer = parseInt(value);
             if(isNaN(integer)) {
-              return 0;
+              return '';
             } else {
               return integer;
             }
