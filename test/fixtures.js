2e5aa542e0cdd8970c18603664543a2a4c0d0daf
Evan Plaice <evanplaice@gmail.com>
2014-05-28 15:03:23 -0700

    Renamed some fixtures and updated fixtures.js
    to_array.csv -> array.csv
    from_array.json -> array.json
    to_arrays.csv -> arrays.csv
    from_arrays.json -> arrays.json

--- test/fixtures.js
+++ test/fixtures.js
@@ -12,10 +12,10 @@ var fs     = require('fs');
   }
 
   var fixtures = {
-    to_array:     CSVfixture('to_array'),
-    from_array:   JSONfixture('from_array'),
-    to_arrays:    CSVfixture('to_arrays'),
-    from_arrays:  JSONfixture('from_arrays'),
+    array_csv:    CSVfixture('array'),
+    array_obj:    JSONfixture('array'),
+    arrays_csv:   CSVfixture('arrays'),
+    arrays_obj:   JSONfixture('arrays'),
     basic_unix:   CSVfixture('basic_unix'),
     basic_dos:    CSVfixture('basic_dos'),
     basic_mac:    CSVfixture('basic_mac')
