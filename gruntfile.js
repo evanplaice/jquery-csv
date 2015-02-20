398ff8cd5a81b1b87105c371965e4983942a5ce2
Evan Plaice <evanplaice@gmail.com>
2015-02-19 22:15:21 -0800

    Fixed a minor issue in the gruntfile
    This isn't a grunt module so there is no tasks folder

--- gruntfile.js
+++ gruntfile.js
@@ -54,7 +54,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-htmlhint');
-  grunt.loadTasks('tasks');
 
   // Default task.
   grunt.registerTask('default', ['jshint']);
