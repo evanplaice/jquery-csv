5c5f758095e27e7fab14a086c0e8246fcbf3deda
Evan Plaice <evanplaice@gmail.com>
2018-01-25 16:31:27 -0700

    Churn, clean node-usage.js

--- examples/node-usage.js
+++ examples/node-usage.js
@@ -1,11 +1,15 @@
+/* global $:true jQuery:true */
+
 var fs = require('fs');
 var $ = jQuery = require('jQuery');
 require('../src/jquery.csv.js');
 
 var sample = './data/sample.csv';
-fs.readFile(sample, 'UTF-8', function(err, csv) {
-  $.csv.toArrays(csv, {}, function(err, data) {
-    for(var i=0, len=data.length; i<len; i++) {
+fs.readFile(sample, 'UTF-8', function (err, csv) {
+  if (err) { console.log(err); }
+  $.csv.toArrays(csv, {}, function (err, data) {
+    if (err) { console.log(err); }
+    for (var i = 0, len = data.length; i < len; i++) {
       console.log(data[i]);
     }
   });
