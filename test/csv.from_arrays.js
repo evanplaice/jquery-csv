cf4122ecb9ef8bb82e8f7dc787d447dcb1face55
Evan Plaice <evanplaice@gmail.com>
2018-01-30 21:51:19 -0700

    Rewrite test $.csv.fromArrays()

---
+++ test/csv.from_arrays.js
@@ -0,0 +1,19 @@
+const test = require('tape');
+const csv = require('../src/jquery.csv.js');
+const fixtures = require('./fixtures/fixtures.js');
+
+const setup = () => {
+  const fixtures = require('./fixtures/fixtures.js');
+  return fixtures;
+};
+
+const teardown = (fixtures) => {
+  fixtures = {};
+};
+
+test('$.csv.fromArrays() - should be able to format multi-entry/multi-cell data set', (t) => {
+  let result = csv.fromArrays(fixtures.arrays_obj);
+  let expect = fixtures.arrays_csv;
+  t.deepEqual(result, expect);
+  t.end();
+})
