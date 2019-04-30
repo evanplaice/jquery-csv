a84a12fa8cb1e425fbace4c22ef9cb94d8100dde
Colton Ehrman <coltonje95@gmail.com>
2019-04-30 15:38:57 -0500

    Fix semistandard errors

--- test/csv.on_post_parse_hook.js
+++ test/csv.on_post_parse_hook.js
@@ -4,26 +4,26 @@ const fixtures = require('./fixtures/fixtures.js');
 
 test('$.csv.toObjects onPostParse hook callback - should be passed the data and state', (t) => {
   let passedData, passedState;
-  
+
   csv.toObjects(fixtures.objects_csv, { onPostParse: (data, state) => {
     passedData = data;
     passedState = state;
-  }});
+  } });
 
-  t.isNot(passedData, null,'data argument should not be null');
+  t.isNot(passedData, null, 'data argument should not be null');
   t.isNot(passedState, null, 'state argument should not be null');
   t.end();
 });
 
 test('$.csv.toArrays onPostParse hook callback - should be passed the data and state', (t) => {
   let passedData, passedState;
-  
+
   csv.toArrays(fixtures.arrays1_csv, { onPostParse: (data, state) => {
     passedData = data;
     passedState = state;
-  }});
+  } });
 
-  t.isNot(passedData, null,'data argument should not be null');
+  t.isNot(passedData, null, 'data argument should not be null');
   t.isNot(passedState, null, 'state argument should not be null');
   t.end();
 });
