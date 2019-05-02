0542bfab71908727d509f5e8df5a9ca3be6ba6c4
Evan Plaice <evanplaice@gmail.com>
2019-05-02 15:23:01 -0600

    Add test

---
+++ test/csv.on_parse_value_hook.js
@@ -0,0 +1,46 @@
+const test = require('tape');
+const csv = require('../src/jquery.csv.js');
+const fixtures = require('./fixtures/fixtures.js');
+
+test('$.csv.toObjects onParseValue hook callback - should be passed the data and state', (t) => {
+  let passedData, passedState;
+
+  csv.toObjects(fixtures.objects_csv, { onParseValue: (data, state) => {
+    passedData = data;
+    passedState = state;
+    return data;
+  } });
+
+  t.isNot(passedData, null, 'data argument should not be null');
+  t.isNot(passedState, null, 'state argument should not be null');
+  t.end();
+});
+
+test('$.csv.toArrays onParseValue hook callback - should be passed the data and state', (t) => {
+  let passedData, passedState;
+
+  csv.toArrays(fixtures.arrays1_csv, { onParseValue: (data, state) => {
+    passedData = data;
+    passedState = state;
+    return data;
+  } });
+
+  t.isNot(passedData, null, 'data argument should not be null');
+  t.isNot(passedState, null, 'state argument should not be null');
+  t.end();
+});
+
+test('$.csv.toObjects onParseValue hook callback - should affect return value', (t) => {
+  const returnMutatedValues = (value) => value + 'a';
+  console.log(fixtures.value_objects_obj);
+  const result = csv.toObjects(fixtures.value_objects_csv, { onParseValue: returnMutatedValues });
+  t.deepEqual(result, fixtures.value_objects_obj, 'return value should reflect what was returned from callback');
+  t.end();
+});
+
+test('$.csv.toArrays onParseValue hook callback - should affect return value', (t) => {
+  const returnMutatedValues = (value) => value + 'a';
+  const result = csv.toArrays(fixtures.value_arrays_csv, { onParseValue: returnMutatedValues });
+  t.deepEqual(result, fixtures.value_arrays_obj, 'return value should reflect what was returned from callback');
+  t.end();
+});
