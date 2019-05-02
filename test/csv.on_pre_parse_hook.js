739aea6976ddad716cfb19f56a3577bfb6cb74ba
Evan Plaice <evanplaice@gmail.com>
2019-05-02 13:55:59 -0600

    Add test

---
+++ test/csv.on_pre_parse_hook.js
@@ -0,0 +1,45 @@
+const test = require('tape');
+const csv = require('../src/jquery.csv.js');
+const fixtures = require('./fixtures/fixtures.js');
+
+test('$.csv.toObjects onPreParse hook callback - should be passed the raw csv and state', (t) => {
+  let passedCSV, passedState;
+
+  csv.toObjects(fixtures.objects_csv, { onPreParse: (csv, state) => {
+    passedCSV = csv;
+    passedState = state;
+    return csv;
+  } });
+
+  t.isNot(passedCSV, null, 'data argument should not be null');
+  t.isNot(passedState, null, 'state argument should not be null');
+  t.end();
+});
+
+test('$.csv.toArrays onPreParse hook callback - should be passed the raw csv and state', (t) => {
+  let passedCSV, passedState;
+
+  csv.toArrays(fixtures.arrays1_csv, { onPreParse: (csv, state) => {
+    passedCSV = csv;
+    passedState = state;
+    return csv;
+  } });
+
+  t.isNot(passedCSV, null, 'data argument should not be null');
+  t.isNot(passedState, null, 'state argument should not be null');
+  t.end();
+});
+
+test('$.csv.toObjects onPreParse hook callback - should affect return value', (t) => {
+  const returnEmptyCSV = () => '';
+  const result = csv.toObjects(fixtures.objects_csv);
+  t.deepEqual(result, fixtures.objects_obj, 'return value should reflect what was returned from callback');
+  t.end();
+});
+
+test('$.csv.toArrays onPreParse hook callback - should affect return value', (t) => {
+  const returnEmptyCSV = () => '';
+  const result = csv.toArrays(fixtures.arrays1_csv, { onPreParse: returnEmptyCSV });
+  t.deepEqual(result, [], 'return value should reflect what was returned from callback');
+  t.end();
+});
